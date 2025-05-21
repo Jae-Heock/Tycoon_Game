24249b499bd533bed2f6ef8c4b5dafba12fc11dd
jjangil <sji906@naver.com>
2025-05-22 00:20:58 +0900

    Test

---
+++ Assets/1Scripts/Test.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+    Player player;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        player = GetComponent<Player>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
